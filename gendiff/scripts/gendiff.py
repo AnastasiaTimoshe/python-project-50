a1bbf870fc8c59c7926c3b1cf7790d0cd13a20ff
AnastasiaTimoshe <a.n.timoshevskaya@gmail.com>
2024-01-21 20:28:44 +0100

    Add JSON format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,23 +2,14 @@
 
 """Gendiff script."""
 
-import argparse
-
-from gendiff import generate_diff
+from gendiff.diff_with_formatter import generate_diff
+from gendiff.cli import parse
 
 
 def main():
-    """Print differences between two files."""
-
-    parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file', metavar='first_file', type=str)
-    parser.add_argument('second_file', metavar='second_file', type=str)
-    parser.add_argument('-f', '--format',
-                        help='set format of output',
-                        default='stylish')
-    args = parser.parse_args()
-
-    print(generate_diff(args.first_file, args.second_file, args.format))
+    args = parse()
+    print(
+        generate_diff(args.first_file, args.second_file, formater=args.format))
 
 
 if __name__ == '__main__':
