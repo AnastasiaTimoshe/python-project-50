41df69db2adcc03f6087652a096eaf6702e8a582
AnastasiaTimoshe <a.n.timoshevskaya@gmail.com>
2024-01-21 22:13:14 +0100

    add gen_difference

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,14 +2,23 @@
 
 """Gendiff script."""
 
-import gendiff
+import argparse
+
+from gendiff import generate_diff
 
 
 def main():
-    args = gendiff.parse_arguments.parse_arguments()
-    print(gendiff.gendiff.generate_diff(args.first_file,
-                                        args.second_file,
-                                        args.output_format))
+    """Print differences between two files."""
+
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file', metavar='first_file', type=str)
+    parser.add_argument('second_file', metavar='second_file', type=str)
+    parser.add_argument('-f', '--format',
+                        help='set format of output',
+                        default='stylish')
+    args = parser.parse_args()
+
+    print(generate_diff(args.first_file, args.second_file, args.format))
 
 
 if __name__ == '__main__':
