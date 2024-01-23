5fec93432bb817d125c4969b17f8357b0f9d061a
AnastasiaTimoshe <a.n.timoshevskaya@gmail.com>
2024-01-23 20:38:45 +0100

    add upd tests/fixtures

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,61 +1,31 @@
 """Gendiff tests"""
 
+from tests.conftest import get_fixture_path
 from gendiff import generate_diff
 import pytest
 
 
-from fixtures.output import RIGHT_JSON, RIGHT_SIMPLE, RIGHT_STYLISH, RIGHT_PLAIN
-
-
-@pytest.mark.parametrize("first_file, second_file, expected_output, format", [
-    (
-        './tests/fixtures/yaml/filepath1_plain.yml',
-        './tests/fixtures/yaml/filepath2_plain.yml',
-        RIGHT_SIMPLE,
-        'stylish'
-    ),
-    (
-        './tests/fixtures/json/file1_plain.json',
-        './tests/fixtures/json/file2_plain.json',
-        RIGHT_SIMPLE,
-        'stylish'
-    ),
-    (
-        './tests/fixtures/json/file1_nest.json',
-        './tests/fixtures/json/file2_nest.json',
-        RIGHT_STYLISH,
-        'stylish'
-    ),
-    (
-        './tests/fixtures/yaml/filepath1_nest.yml',
-        './tests/fixtures/yaml/filepath2_nest.yml',
-        RIGHT_STYLISH,
-        'stylish'
-    ),
-    (
-        './tests/fixtures/json/file1_nest.json',
-        './tests/fixtures/json/file2_nest.json',
-        RIGHT_PLAIN,
-        'plain'
-    ),
-    (
-        './tests/fixtures/yaml/filepath1_nest.yml',
-        './tests/fixtures/yaml/filepath2_nest.yml',
-        RIGHT_PLAIN,
-        'plain'
-    ),
-    (
-        './tests/fixtures/json/file1_nest.json',
-        './tests/fixtures/json/file2_nest.json',
-        RIGHT_JSON,
-        'json'
-    ),
-    (
-        './tests/fixtures/yaml/filepath1_nest.yml',
-        './tests/fixtures/yaml/filepath2_nest.yml',
-        RIGHT_JSON,
-        'json'
-    ),
-])
-def test_gendiff(first_file, second_file, expected_output, format):
-    assert generate_diff(first_file, second_file, format) == expected_output
+@pytest.mark.parametrize(
+    "input_file1,input_file2,output_file,formater",
+    [
+        ("file1.json", "file2.json", "result.txt", 'stylish'),
+        ("file1.json", "file2.json", "result.txt", None),
+        ("file1.yaml", "file2.yaml", "result.txt", 'stylish'),
+        ("file1.yaml", "file2.yaml", "result.txt", None),
+        ("file1-rec.json", "file2-rec.json", "result-rec.txt", 'stylish'),
+        ("file1-rec.json", "file2-rec.json", "result-rec.txt", None),
+        ("file1-rec.yaml", "file2-rec.yaml", "result-rec.txt", 'stylish'),
+        ("file1-rec.yaml", "file2-rec.yaml", "result-rec.txt", None),
+        ("file1-rec.json", "file2-rec.json", "result-plain.txt", 'plain'),
+        ("file1-rec.yaml", "file2-rec.yaml", "result-plain.txt", 'plain'),
+        ("file1.json", "file2.json", "result-json.txt", 'json'),
+        ("file1.yaml", "file2.yaml", "result-json.txt", 'json'),
+    ]
+)
+def test_gendiff(input_file1, input_file2, output_file, formater):
+    output = get_fixture_path(output_file)
+    with open(output) as result_file:
+        result = result_file.read()
+        file1 = get_fixture_path(input_file1)
+        file2 = get_fixture_path(input_file2)
+        assert generate_diff(file1, file2, formater) == result
