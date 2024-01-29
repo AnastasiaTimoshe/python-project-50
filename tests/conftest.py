761b2771a812c40e7ab4e7a1abd8feb8912d0784
AnastasiaTimoshe <a.n.timoshevskaya@gmail.com>
2024-01-29 01:02:16 +0100

    add .

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,3 +3,8 @@ import os
 
 def get_fixture_path(file):
     return os.path.join('tests', 'fixtures', file)
+
+
+# def pytest_assertrepr_compare(config, op, left, right):
+#     if op in ('==', '!='):
+#         return ['{0} {1} {2}'.format(left, op, right)]
